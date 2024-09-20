2d50784cb97ce1803af476583bf5aae7032940d6
unknown <sigmaligma@skibidi.com>
2024-09-20 14:40:19 +0300

    auth started

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = User::create($fields);
+
+        return $user;
+    }
+
+    public function login(Request $request)
+    {
+        return 'login';
+    }
+
+    public function logout(Request $request)
+    {
+        return 'logout';
+    }
+}
